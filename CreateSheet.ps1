e597a8e6de043d4de9fb3971262c59d734c101af
Markus Richtsfeld <markus.richtsfeld@siemens-healthineers.com>
2022-11-30 09:47:12 +0100

    Now with solution sheet as well ;)

--- CreateSheet.ps1
+++ CreateSheet.ps1
@@ -1,6 +1,7 @@
 $Aufgaben = 10
 
 function Addition {
+    [OutputType([System.Int32[]])]
     $min = 10
     $max = 5000
     $a = Get-Random -Maximum $max -Minimum $min
@@ -10,34 +11,38 @@ function Addition {
 }
 
 function Subtratktion {
+    [OutputType([System.Int32[]])]
     $min = 10
     $max = 5000
     $a = Get-Random -Maximum $max -Minimum $min
     $b = Get-Random -Maximum $max -Minimum $min
     
-    if($a -ge $b) {
+    if ($a -ge $b) {
         return @($a, $b)
-    } else {
+    }
+    else {
         return ($b, $a)
     }
 }
 
 function Mulitplikation {
+    [OutputType([System.Int32[]])]
     $min = 10
     $max = 500
     $a = Get-Random -Maximum $max -Minimum $min
-    $b = Get-Random -Maximum $($max/10) -Minimum 3
+    $b = Get-Random -Maximum $($max / 10) -Minimum 3
     return @($a, $b)
 }
 
 function Division {
+    [OutputType([System.Int32[]])]
     $min = 10
     $max = 500
     do {
         $a = Get-Random -Maximum $max -Minimum $min
-        $b = Get-Random -Maximum $($max/10) -Minimum 2
+        $b = Get-Random -Maximum $($max / 10) -Minimum 2
 
-        if($b -gt $a) {
+        if ($b -gt $a) {
             $c = $a
             $a = $b
             $b = $c
@@ -47,52 +52,89 @@ function Division {
     return @($a, $b)
 }
 
-$dt = New-Object System.Data.DataTable
-$dt.Columns.Add("Zahl 1") | Out-Null
-$dt.Columns.Add("opp") | Out-Null
-$dt.Columns.Add("Zahl 2") | Out-Null
-$dt.Columns.Add("Ist gleich") | Out-Null
+function Get-Result {
+    param (
+        [Parameter(Mandatory=$true, Position=1)] [int] $a,
+        [Parameter(Mandatory=$true, Position=2)] [int] $b,
+        [Parameter(Mandatory=$true, Position=3)] [string] $opp
+    )
 
-for($i=1; $i -le $Aufgaben; $i++) {
+    Begin {}
+
+    Process {
+        Write-Host "$($a) $($opp) $($b)"
+        switch ($opp) {
+            "+" { Write-Host "+"; return $($a + $b) }
+            "-" { Write-Host "-"; return $($a - $b) }
+            "*" { Write-Host "*"; return $($a * $b) }
+            ":" { Write-Host "/"; return $($a / $b) }
+            default { return 0 }
+        }
+
+        throw "Kacke"
+    }
+
+    End {}
+}
+
+$dt = New-Object System.Data.DataTable
+$dt.Columns.Add("NumberA", [System.Int32]) | Out-Null
+$dt.Columns.Add("Opp", [System.String]) | Out-Null
+$dt.Columns.Add("NumberB", [System.Int32]) | Out-Null
+
+for ($i = 1; $i -le $Aufgaben; $i++) {
     ($a, $b) = Addition
     $r = $dt.NewRow()
-    $r[0] = $a
-    $r[1] = "+"
-    $r[2] = $b
-    $r[3] = "="
-    $dt.Rows.Add($r);
+    [int]$r[0] = $a
+    $r[1] = '+'
+    [int]$r[2] = $b
+    $dt.Rows.Add($r)
 }
 
-for($i=1; $i -le $Aufgaben; $i++) {
+for ($i = 1; $i -le $Aufgaben; $i++) {
     ($a, $b) = Subtratktion
     $r = $dt.NewRow()
-    $r[0] = $a
-    $r[1] = "-"
-    $r[2] = $b
-    $r[3] = "="
-    $dt.Rows.Add($r);
+    [int]$r[0] = $a
+    $r[1] = '-'
+    [int]$r[2] = $b
+    $dt.Rows.Add($r)
 }
 
-for($i=1; $i -le $Aufgaben; $i++) {
+for ($i = 1; $i -le $Aufgaben; $i++) {
     ($a, $b) = Mulitplikation
     $r = $dt.NewRow()
-    $r[0] = $a
-    $r[1] = "*"
-    $r[2] = $b
-    $r[3] = "="
-    $dt.Rows.Add($r);
+    [int]$r[0] = $a
+    $r[1] = '*'
+    [int]$r[2] = $b
+    $dt.Rows.Add($r)
 }
 
-for($i=1; $i -le $Aufgaben; $i++) {
+for ($i = 1; $i -le $Aufgaben; $i++) {
     ($a, $b) = Division
     $r = $dt.NewRow()
-    $r[0] = $a
-    $r[1] = ":"
-    $r[2] = $b
-    $r[3] = "="
-    $dt.Rows.Add($r);
+    [int]$r[0] = $a
+    $r[1] = ':'
+    [int]$r[2] = $b
+    $dt.Rows.Add($r)
 }
 
-$dt | Export-Csv -Path "Übungsblatt.csv" -Force -NoTypeInformation -Delimiter ";"
+$dt | Select-Object -Property `
+    @{ name = "a"; expr = { $_."NumberA" } }, `
+    "Opp", `
+    @{ name = "b"; expr = { $_."NumberB" } }, `
+    @{ name = '='; expr = { "=" } } `
+    | Export-Csv -Path "Exercises.csv" -Force -NoTypeInformation -Delimiter ";"
 
 
+$dt | Select-Object -Property `
+    @{ name = "a"; expr = { $_.NumberA } }, `
+    "Opp", `
+    @{ name = "b"; expr = { $_.NumberB } }, `
+    @{ name = '='; expr = { "=" } }, `
+    @{ name = "Result"; expr = { Get-Result -a $_.NumberA -b $_.NumberB -opp $_.Opp }} `
+    | Export-Csv -Path "Solution.csv" -Force -NoTypeInformation -Delimiter ";"
+
+
+foreach($r in $dt) {
+    Get-Result $r.NumberA $r.NumberB $r.Opp
+}
